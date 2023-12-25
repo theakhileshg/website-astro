71f01470c73fb7362e96648d82039484f3079c32
AKHILESH <theakhileshg@gmail.com>
2023-12-25 09:39:41 +0000

    Meta data changes

--- src/consts.ts
+++ src/consts.ts
@@ -4,12 +4,12 @@
  * A few of these are not used yet, and are subject to change, example of this is Author.
  */
 export const SITE_METADATA = {
-    title: 'Astro Starter Blog',
-    headerTitle: 'Astro Starter Blog',
+    title: 'Akhilesh G',
+    headerTitle: 'Akhilesh G',
     description: 'A blog created with Astro and Tailwind.css',
     language: 'en-us',
     theme: 'system', // Options: system, light, dark, Does not work yet
-    siteUrl: 'https://astro-starter-blog.yon.fun/',
+    siteUrl: 'blog.akhileshg.com',
     siteRepo: 'https://github.com/wanoo21/tailwind-astro-starting-blog',
     siteLogo: '/static/images/logo.png',
     socialBanner: '/static/images/twitter-card.png',
@@ -17,7 +17,7 @@ export const SITE_METADATA = {
 
     // The following are subject to change. They are placeholders for now.
     // This project provide a default author content see src/content/authors/default.mdx, so these details are better to be inserted there.
-    author: 'John Doe',
+    author: 'Akhilesh G',
     mastodon: 'https://mastodon.social/@mastodonuser',
     email: 'address@yoursite.com',
     github: 'https://github.com',
